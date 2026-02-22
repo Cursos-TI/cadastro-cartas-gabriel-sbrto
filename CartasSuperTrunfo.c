85e3833520ede4d38e68606adcd387397d733c40
gabriel-sbrto <gabrielsbrto@gmail.com>
2026-02-22 15:00:01 +0000

    Adicionando variaveis e primeiros scanfs

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,44 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+//variaveis para as cartas 1 e 2
+  char estado1;
+  char codigo1[4];
+  char cidade1[50];
+  int populacao1;
+  float area1, pib1;
+  int pontos1;
 
-  // Área para entrada de dados
+  char estado2;
+  char codigo2[4];
+  char cidade2;
+  int populacao2;
+  float area2, pib2;
+  int pontos2;
 
-  // Área para exibição dos dados da cidade
+  //leitura de carta 1 e 2
+  printf("Digite o Estado (A-H): ");
+  scanf(" %c", &estado1);
 
-return 0;
+  printf("Digite o Codigo (ex: A01): ");
+  scanf("%s", cidade1);
+
+  printf("Nome da Cidade: ");
+  scanf("%s", &cidade1);
+
+  printf("Populacao: ");
+  scanf("%d", &populacao1);
+
+  printf("Area (Km2): ");
+  scanf("%f", area1);
+
+  printf("PIB: ");
+  scanf("%f", &pib1);
+  
+  printf("Numero de Pontos Turisticos: ");
+  scanf("%d", pontos1);
+
+  
+ 
+
 } 
